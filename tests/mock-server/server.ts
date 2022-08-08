c5b1d537796c93d0d3b900b56382cc972f8d4692
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2022-08-08 19:47:38 -0400

    cleanup

--- tests/mock-server/server.ts
+++ tests/mock-server/server.ts
@@ -17,40 +17,48 @@ export function setup(): Promise<boolean> {
         reply.send(request.headers)
     })
 
-    type NdjsonRequest = FastifyRequest<{
-        Params: {
-            site: string
-            file: string
-        }
-    }>
+    fastify.get(
+        '/ndjson/:site/:file',
+        (
+            request: FastifyRequest<{
+                Params: {
+                    site: string
+                    file: string
+                }
+            }>,
+            reply
+        ) => {
+            if (request.headers.accept !== 'application/x-ndjson') {
+                reply.status(400).send({
+                    error: 'Missing header: `Accept: application/x-ndjson`',
+                })
+                return
+            }
 
-    fastify.get('/ndjson/:site/:file', (request: NdjsonRequest, reply) => {
-        if (request.headers.accept !== 'application/x-ndjson') {
-            reply.status(400).send({
-                error: 'Missing header: `Accept: application/x-ndjson`',
+            const { site, file } = request.params
+
+            reply.type('application/x-ndjson').send(createReadStream(__dirname + `/data/${site}/${file}.ndjson`))
+        }
+    )
+
+    fastify.get(
+        '/status/:code',
+        (
+            request: FastifyRequest<{
+                Params: {
+                    code: number
+                }
+            }>,
+            reply
+        ) => {
+            reply.status(request.params.code).send({
+                error: 'Error',
+                code: request.params.code,
             })
-            return
         }
+    )
 
-        const { site, file } = request.params
-
-        reply.type('application/x-ndjson').send(createReadStream(__dirname + `/data/${site}/${file}.ndjson`))
-    })
-
-    type StatusCodeRequest = FastifyRequest<{
-        Params: {
-            code: number
-        }
-    }>
-
-    fastify.get('/status/:code', (request: StatusCodeRequest, reply) => {
-        reply.status(request.params.code).send({
-            error: 'Error',
-            code: request.params.code,
-        })
-    })
-
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         fastify.listen({ port: 0 }, (err, address) => {
             if (err) throw err
 
