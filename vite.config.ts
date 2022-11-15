966e1da4b31e524309a66cbf8f323a9e694a2543
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2022-11-14 22:08:32 -0500

    use c8 for coverage

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     test: {
         globalSetup: 'tests/mock-server/server.ts',
-        coverage: {
-            provider: 'istanbul',
-        },
     },
 })
